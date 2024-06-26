f32ae66b451024efa45948d19751483becbb3370
eSig <mm140646@gmail.com>
2024-06-26 18:51:58 +0300

    first commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,8 @@
 <x-layout>
     @foreach ($posts as $post)
-        @dd($post);
         <article>
             <h1>
-                <a href="posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->slug }}">
                     {!! $post->title !!}
                 </a>
             </h1>
